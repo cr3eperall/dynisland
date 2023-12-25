b062f1be7374fc401a628d63180fb088cf682357
cr3eperall <davide.michelotti04@gmail.com>
2023-12-25 19:32:10 +0100

    added minimal_height to config, css reloading, added border and bg to ScrollingLabel

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,15 @@
-use std::{collections::HashMap, rc::Rc};
-
 use anyhow::{Context, Result};
-use dynisland::{
-    app::{self, App},
-    config,
-};
-use tokio::sync::Mutex;
+use dynisland::app::App;
 
 extern crate dynisland_modules; //need this to force the modules to be linked
+
+// [ ]  TODO remove some unnecessary arc and mutexes
+// [ ] TODO remove some unwraps and handle errors better
 
 fn main() -> Result<()> {
     //init GTK
     gtk::init().with_context(|| "failed to init gtk")?;
-    let (hdl, shutdown) = app::get_new_tokio_rt(); //TODO it's ugly, should change it
-    let app = App {
-        window: app::get_window(),
-        module_map: Rc::new(Mutex::new(HashMap::new())), //TODO remove some unnecessary arc and mutexes
-        producers_handle: hdl,
-        producers_shutdown: shutdown,
-        app_send: None,
-        config: config::Config::default(),
-    };
+    let app = App::default();
     app.initialize_server()?;
     Ok(())
 }
