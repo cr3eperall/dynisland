ce6ea02cfea2565637860e46e095d297103996f3
cr3eperall <davide.michelotti04@gmail.com>
2024-02-22 19:10:59 +0100

    remove css-anim, switch to stable rust

--- dynisland-core/src/lib.rs
+++ dynisland-core/src/lib.rs
@@ -1,8 +1,3 @@
-#![feature(try_blocks)]
-#![feature(div_duration)]
-#![feature(lazy_cell)]
-#![feature(trait_upcasting)]
-#![feature(type_name_of_val)]
-
 pub mod base_module;
 pub mod graphics;
+pub mod module_abi;
