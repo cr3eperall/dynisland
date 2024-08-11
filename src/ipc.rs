a0900b18ad0fe8cf6d584e90a75db986f796818f
cr3eperall <davide.michelotti04@gmail.com>
2024-08-11 18:18:22 +0200

    add cli, daemon mode, log file, move module loading logic, update paths

---
+++ src/ipc.rs
@@ -0,0 +1,61 @@
+use std::{io::Write, path::Path};
+
+use anyhow::{Ok, Result};
+use tokio::{io::AsyncReadExt, net::{UnixListener, UnixStream}, sync::mpsc::UnboundedSender};
+
+use crate::{app::BackendServerCommand, cli::SubCommands};
+
+
+pub async fn open_socket(runtime_path: &Path, server_send: UnboundedSender<BackendServerCommand>) -> Result<()>{
+    let listener = UnixListener::bind(runtime_path.join("dynisland.sock"))?;
+    loop {
+        let (stream, _socket)=listener.accept().await?;
+        match read_message(stream).await?{
+            SubCommands::Reload => {
+                server_send.send(BackendServerCommand::ReloadConfig)?;
+            },
+            SubCommands::Inspector => {
+                server_send.send(BackendServerCommand::OpenInspector)?;
+            },
+            SubCommands::Kill => {
+                server_send.send(BackendServerCommand::Stop)?;
+                break;
+            },
+            SubCommands::HealthCheck => {
+                log::info!("Recieved HealthCheck, Everything OK");
+            }
+
+            SubCommands::DefaultConfig { replace_current_config: _ } |
+            SubCommands::Daemon { no_daemonize: _ } |
+            SubCommands::Restart { no_daemonize: _ }=> {
+                log::error!("invalid message passed to ipc");
+            },
+        }
+    }
+
+    Ok(())
+}
+
+pub async fn read_message(mut stream: UnixStream)-> Result<SubCommands>{
+
+    let mut message_len_bytes=[0u8; 4];
+    stream.read_exact(&mut message_len_bytes).await?;
+    let message_len=u32::from_be_bytes(message_len_bytes) as usize;
+    let mut message: Vec<u8> = Vec::with_capacity(message_len);
+    while message.len() < message_len {
+        stream.read_buf(&mut message).await?;
+    }
+
+    Ok(bincode::deserialize(&message)?)
+}
+
+pub fn send_message(mut stream: std::os::unix::net::UnixStream, message: &SubCommands)-> Result<()>{
+    stream.set_nonblocking(false)?;
+
+    let message = bincode::serialize(&message)?;
+    let message_len_bytes=(message.len() as u32).to_be_bytes();
+    stream.write_all(&message_len_bytes)?;
+    stream.write_all(&message)?;
+
+    Ok(())
+}
--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
+use std::{
+    io::ErrorKind,
+    os::{fd::AsRawFd, unix::net::UnixStream},
+    path::Path,
+    thread,
+    time::Duration,
+};
+
 use anyhow::{Context, Result};
-use dynisland::app::App;
+use clap::Parser;
+use dynisland::{app::App, cli::Cli, config};
+use dynisland::{
+    cli::SubCommands::{self, *},
+    ipc,
+};
+use dynisland_abi::module::UIServerCommand;
 use env_logger::Env;
 use log::Level;
+use nix::unistd::Pid;
 
 // [ ] TODO remove some unnecessary arc and mutexes
 // [ ] TODO remove some unwraps and handle errors better
@@ -15,9 +30,6 @@ use log::Level;
 // the backtrace is in backtrace.txt
 
 fn main() -> Result<()> {
-    //init GTK
-    gtk::init().with_context(|| "failed to init gtk")?;
-
     env_logger::Builder::new()
         // .filter_module("dynisland", log::LevelFilter::Debug)
         // .filter_module("dynisland_core", log::LevelFilter::Debug)
@@ -25,7 +37,148 @@ fn main() -> Result<()> {
         .parse_env(Env::default().default_filter_or(Level::Info.as_str()))
         .init();
 
-    let app = App::default();
-    app.initialize_server()?;
+    let cli = Cli::parse();
+    let config_dir = cli
+        .config_path
+        .clone()
+        .unwrap_or(config::get_default_config_path());
+    let config = config::get_config(&config_dir);
+    log::debug!("{cli:?}");
+    match cli.command {
+        Daemon { no_daemonize } => {
+            let pid = if !no_daemonize {
+                let runtime_dir = config.get_runtime_dir();
+                if let Ok(stream) = UnixStream::connect(runtime_dir.join("dynisland.sock")) {
+                    match ipc::send_message(stream, &HealthCheck) {
+                        Ok(_) => {
+                            //app is already runnig
+                            log::error!("Application is already running");
+                        }
+                        Err(_) => {
+                            log::error!("Error sending HealthCheck");
+                        }
+                    };
+                    return Ok(());
+                } else {
+                    let _ = std::fs::remove_file(runtime_dir.join("dynisland.sock"));
+                }
+                let path = runtime_dir.join("dynisland.log");
+                detach(&path)?
+            } else {
+                Pid::from_raw(std::process::id() as i32)
+            };
+            //init GTK
+            gtk::init().with_context(|| "failed to init gtk")?;
+            let app = App::default();
+            log::info!("pid: {pid}");
+            app.initialize_server(&config_dir)?;
+        }
+        Reload | Kill | Inspector | HealthCheck => {
+            let socket_path = config.get_runtime_dir().join("dynisland.sock");
+            match UnixStream::connect(socket_path.clone()) {
+                Ok(stream) => {
+                    ipc::send_message(stream, &cli.command)?;
+                }
+                Err(err) => {
+                    if cli.command != Kill {
+                        log::error!("Error opening dynisland socket: {err}");
+                    }
+                    if cli.command == HealthCheck {
+                        println!("OK");
+                    }
+                    if matches!(err.kind(), ErrorKind::ConnectionRefused) {
+                        log::info!("Connection refused, deleting old socket file");
+                        std::fs::remove_file(socket_path.clone())?;
+                    }
+                }
+            };
+        }
+        Restart { no_daemonize } => {
+            let socket_path = config.get_runtime_dir().join("dynisland.sock");
+            match UnixStream::connect(socket_path.clone()) {
+                Ok(stream) => {
+                    ipc::send_message(stream, &SubCommands::Kill)?;
+                    log::info!("Waiting for daemon to die");
+                    let mut tries=0;
+                    while socket_path.exists() && tries < 10{
+                        thread::sleep(Duration::from_millis(500));
+                        print!(".");
+                        tries += 1;
+                    }
+                    println!();
+                    if tries==10{
+                        log::error!("failed to stop the old instance, manual kill needed");
+                    }
+                }
+                Err(err) => {
+                    log::error!("Error opening dynisland socket: {err}");
+                    if matches!(err.kind(), ErrorKind::ConnectionRefused) {
+                        log::info!("Connection refused, trying to delete old socket file");
+                        std::fs::remove_file(socket_path.clone())?;
+                    }
+                }
+            };
+
+            let pid = if !no_daemonize {
+                let path = config.get_runtime_dir().join("dynisland.log");
+                detach(&path)?
+            } else {
+                Pid::from_raw(std::process::id() as i32)
+            };
+            //init GTK
+            gtk::init().with_context(|| "failed to init gtk")?;
+            let app = App::default();
+            log::info!("pid: {pid}");
+            app.initialize_server(&config_dir)?;
+        }
+        DefaultConfig {
+            replace_current_config,
+        } => {
+            gtk::init().with_context(|| "failed to init gtk")?;
+            let mut app = App::default();
+            let (abi_app_send, _abi_app_recv) =
+                abi_stable::external_types::crossbeam_channel::unbounded::<UIServerCommand>();
+            app.app_send = Some(abi_app_send);
+            let (_conf, conf_str) = app.get_default_config();
+            println!("Default Config: \n{conf_str}");
+            if replace_current_config {
+                todo!();
+            }
+        }
+    }
     Ok(())
+}
+
+fn detach(log_file_path: &Path) -> Result<Pid> {
+    std::fs::create_dir_all(log_file_path.parent().expect("invalid log path"))?;
+    let file = std::fs::OpenOptions::new()
+        .create(true)
+        .write(true)
+        .append(false)
+        .truncate(true)
+        .open(log_file_path)
+        .unwrap_or_else(|err| {
+            panic!(
+                "Error opening log file ({}), for writing: {err}",
+                log_file_path.to_string_lossy()
+            )
+        });
+    let fd = file.as_raw_fd();
+
+    // detach from terminal
+    let pid = match unsafe { nix::unistd::fork()? } {
+        nix::unistd::ForkResult::Child => nix::unistd::setsid(),
+        nix::unistd::ForkResult::Parent { .. } => {
+            // nix::unistd::daemon(false, false);
+            std::process::exit(0);
+        }
+    }?;
+
+    if nix::unistd::isatty(1)? {
+        nix::unistd::dup2(fd, std::io::stdout().as_raw_fd())?;
+    }
+    if nix::unistd::isatty(2)? {
+        nix::unistd::dup2(fd, std::io::stderr().as_raw_fd())?;
+    }
+    Ok(pid)
 }
