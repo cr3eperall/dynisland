a3f0cd1d8cd8fcbe5026fb10784d51ecf53c3983
cr3eperall <davide.michelotti04@gmail.com>
2024-08-28 21:43:03 +0200

    fix: update build script to generate completions in the correct directory

--- build.rs
+++ build.rs
@@ -1,5 +1,4 @@
 use std::io::Error;
-
 #[cfg(feature = "completions")]
 use clap::{Command, CommandFactory};
 #[cfg(feature = "completions")]
@@ -10,13 +9,19 @@ include!("src/cli.rs");
 fn main() -> Result<(), Error> {
     #[cfg(feature = "completions")]
     {
-        let outdir = "../target";
+        let outdir = if PathBuf::from("../target").exists(){
+            "../target/"
+        }else if PathBuf::from("target").exists(){
+            "./target/"
+        }else{
+            return Ok(());
+        };
         let bin_name = "dynisland";
 
         let mut cmd: Command = Cli::command_for_update();
         let shells: [Shell; 4] = [Shell::Bash, Shell::Zsh, Shell::Fish, Shell::Elvish];
         for shell in shells {
-            let _path = generate_to(shell, &mut cmd, bin_name, outdir)?;
+            let _path = generate_to(shell, &mut cmd, bin_name, &outdir)?;
 
             // println!(
             //     "cargo:warning=completion file for {shell} is generated in {}",
